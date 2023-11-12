7f02bcaa7b60f590d31c5cc399c0a0bca7bf9d96
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-11-12 23:07:20 +0600

    stack in a problem

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ const  Hero = () => {
           <div className="-mt-36">
             <div ref={ref} className="flex cursor-default flex-col space-y-2">
               <h1 className="text-5xl font-semibold sm:text-7xl md:text-8xl xl:text-7xl">
-                Md. Bahadur mia (Bms)
+                Bahadur Mia Shihab (Bms)
               </h1>
               <h2 className="text-3xl font-medium opacity-80 sm:text-6xl md:text-6xl xl:text-5xl">
                 A front-end Developer
